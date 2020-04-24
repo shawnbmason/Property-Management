33d135edf1a89c07f65e9d3e049bb7b440064567
shawnbmason <s.mason1993@yahoo.com>
2020-04-23 21:19:23 -0400

    setup style and header grid

---
+++ src/components/headerWrapper.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HeaderWrapper extends Component {
+  render() {
+    return (
+      <div className='header-wrapper'>
+        <h1 className='header-wrapper__title'>Welcome to HOA Manager!</h1>
+        <p className='header-wrapper__subtitle'>Please login to continue</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default HeaderWrapper;
