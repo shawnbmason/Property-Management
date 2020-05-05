53cba5619ee01284cb59f550e351864618799523
shawnbmason <s.mason1993@yahoo.com>
2020-05-04 21:20:04 -0400

    login Field component, text-link component, and remaining signin styles

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -4,10 +4,10 @@ import React, { Component } from 'react';
     render() {
       const { className, title, input, type, placeholder } = this.props;
       return (
-        <div className='form-input'>
+        <div className={`${className} form-input`}>
           <label className='form-input__title'>{title}</label>
           <input
-            className={`${className} form-input__input`}
+            className= 'form-input__input'
             type={type}
             {...input}
             placeholder={placeholder}
@@ -16,3 +16,20 @@ import React, { Component } from 'react';
       )
     }
   }
+
+    export class FormButton extends Component {
+      render() {
+        const { className, title, input, type } = this.props;
+        return (
+          <div className={`${className} form-button`}>
+            <button
+              className= 'form-button__button'
+              type={type}
+              {...input}
+            >
+              {title}
+            </button>
+          </div>
+        )
+      }
+    }
