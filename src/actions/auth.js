20f695cb669f42af0d9c9a8f2e7a34ccf1c8172f
shawnbmason <s.mason1993@yahoo.com>
2020-05-21 19:52:09 -0400

    built signin functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -11,12 +11,32 @@ export function signUp(fields, success) {
   return function(dispatch) {
     axios.post(`${ROOT_URL}/signup`, fields)
       .then(response => {
+        const { token } = response.data;
+        localStorage.setItem('token', token);
         dispatch({
           type: AUTHENTICATE_USER,
           payload: response.data
         })
-        success()
+        success();
         //do somthing with that response
+      })
+      .catch(err => {
+        if(err) { console.log(err) }
+      })
+  }
+}
+
+export function signIn(fields, success) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signIn`, fields)
+      .then(response => {
+        const { token } = response.data;
+        localStorage.setItem('token', token);
+        dispatch({
+          type: AUTHENTICATE_USER,
+          payload: response.data
+        })
+        success();
       })
       .catch(err => {
         if(err) { console.log(err) }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
 
 import SigninForm from './signinForm';
 
 class Signin extends Component {
+
+  onSubmit = (fields) => {
+    this.props.signIn(fields, () => {
+      this.props.history.push('/dashboard');
+    })
+  }
+  onSubmit = (fields) => {
+    console.log('trying to handle submit');
+  }
+
+
   render() {
     return (
       <div className='sign-in'>
-        <SigninForm/>
+        <SigninForm onSubmit={() => this.onSubmit(event)}/>
       </div>
     )
   }
 }
 
-export default Signin;
+export default connect(null, actions)(Signin);
