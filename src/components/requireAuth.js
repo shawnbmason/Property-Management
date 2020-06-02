0d8d3a4091fb717e1de43d01bcdea0c538bdb173
shawnbmason <s.mason1993@yahoo.com>
2020-06-02 18:54:26 -0400

    tabnav grid

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -16,7 +16,7 @@ export default function(ComposedComponent) {
       }
     }
     render() {
-      return <ComponentComponent/>
+      return <ComposedComponent/>
     }
   }
 
---
+++ src/components/tabnav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TabNav extends Component {
+  render() {
+    return (
+      <div className='tab-nav'>
+      </div>
+    )
+  }
+}
+
+export default TabNav;
