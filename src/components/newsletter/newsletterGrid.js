f5a58aafb8893326e373adb5dddb98da67b7da5c
shawnbmason <s.mason1993@yahoo.com>
2020-06-08 21:44:33 -0400

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -6,12 +6,21 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
   render() {
+
+    const latest = {
+      _id: '1564',
+      title: 'Happy Holidays Fam',
+      body: 'In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.',
+      date: new Date(),
+      imageUrl: 'http://via.placeholder.com/960x250'
+    }
+
     return (
       <div className='newsletter-grid'>
         {/* add button */}
         <NewsletterBox date={new Date()}/>
         <NewsletterArchive/>
-        <NewsletterLatest/>
+        <NewsletterLatest {...latest}/>
       </div>
     )
   }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,25 +2,21 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
   render() {
+    const { title, imageUrl, body } = this.props;
     return (
       <div className='newsletter-latest'>
-        <h1 className='newsletter-latest__title'>Title goes here</h1>
-        <img className='newsletter-latest__image' src='http://via.placeholder.com/960x250'/>
+        <h1 className='newsletter-latest__title'>{title}</h1>
+        <img className='newsletter-latest__image' src={imageUrl}/>
         <div className='newsletter-latest__body'>
-        <p>Enim mentitum eiusmod. Constias labore iis admodum philosophari, duis ea ubi
-        velit aliquip, est deserunt do incididunt, do do fugiat magna quis a eu non
-        tamen arbitror ad est quem quis illum nescius, proident despicationes est
-        iudicem ita aute cupidatat hic offendit. Qui esse commodo eruditionem. De
-        doctrina comprehenderit id est sed quae mentitum, in quae doctrina ullamco ab
-        pariatur tempor an voluptate tractavissent est an sunt litteris despicationes,
-        ab nostrud sempiternum, proident ab nulla o pariatur dolore mentitum.Aliqua
-        exercitation voluptate amet mandaremus. Aute proident o imitarentur in deserunt
-        aut velit. Ex ad quid elit dolore non aut in nisi fore ipsum quo dolore
-        coniunctione fabulas summis quibusdam, ab quem quorum dolor occaecat.</p>
+        <p>{body}</p>
         </div>
       </div>
     )
   }
 }
+
+// function mapStateToProps(state) {
+//
+// }
 
 export default NewsletterLatest;
