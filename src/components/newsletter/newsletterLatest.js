96712118af40236505ed92f9da0ecae3c3681b24
shawnbmason <s.mason1993@yahoo.com>
2020-06-08 21:19:33 -0400

    styled newsletter latest and applied row gaps to the newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -7,7 +7,16 @@ class NewsletterLatest extends Component {
         <h1 className='newsletter-latest__title'>Title goes here</h1>
         <img className='newsletter-latest__image' src='http://via.placeholder.com/960x250'/>
         <div className='newsletter-latest__body'>
-            <p>Lorem Ipsum</p>
+        <p>Enim mentitum eiusmod. Constias labore iis admodum philosophari, duis ea ubi
+        velit aliquip, est deserunt do incididunt, do do fugiat magna quis a eu non
+        tamen arbitror ad est quem quis illum nescius, proident despicationes est
+        iudicem ita aute cupidatat hic offendit. Qui esse commodo eruditionem. De
+        doctrina comprehenderit id est sed quae mentitum, in quae doctrina ullamco ab
+        pariatur tempor an voluptate tractavissent est an sunt litteris despicationes,
+        ab nostrud sempiternum, proident ab nulla o pariatur dolore mentitum.Aliqua
+        exercitation voluptate amet mandaremus. Aute proident o imitarentur in deserunt
+        aut velit. Ex ad quid elit dolore non aut in nisi fore ipsum quo dolore
+        coniunctione fabulas summis quibusdam, ab quem quorum dolor occaecat.</p>
         </div>
       </div>
     )
