003e2fdee807197e36245dd838d79d9c526dc8e4
shawnbmason <s.mason1993@yahoo.com>
2020-06-23 20:40:46 -0400

    built newsletter detail route and component and setup to newsletter id via req.params

---
+++ src/components/newsletter/newsletterDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterDetail extends Component {
+  render() {
+    return (
+      <div className='newsletter-detail'>
+          newsletter detail
+      </div>
+    )
+  }
+}
+
+export default NewsletterDetail;
