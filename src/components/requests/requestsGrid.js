7e6818dac9c510941b9a27f580ab6f0379fc4926
shawnbmason <s.mason1993@yahoo.com>
2020-06-25 20:07:20 -0400

    built requests components and placed on grid

--- src/components/requests/requestsGrid.js
+++ src/components/requests/requestsGrid.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 
 import Button from '../button';
 
+import RequestsBoxes from './requestsBoxes';
+import Requests from './requests';
+
 class RequestsGrid extends Component {
 
-  handleAddNewsletter = () => {
-    this.props.history.push('/requests/new');
-  }
+    handleAddRequest = () => {
+        this.props.history.push('/request/new');
+    }
 
-  render() {
-    return (
-      <div className='requests-grid'>
-          <Button className='requests-grid__button' icon='fas fa-plus' callback={() => this.handleAddRequests()}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='requests-grid'>
+                <Button className='requests-grid__button' icon='fas fa-plus' callback={() => this.handleAddRequest()}/>
+                <RequestsBoxes/>
+                <Requests/>
+            </div>
+        )
+    }
 }
 
 export default RequestsGrid;
