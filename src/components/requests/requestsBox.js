9001d166684cda8afceb78543b4c8a12efb742cd
shawnbmason <s.mason1993@yahoo.com>
2020-06-30 19:47:18 -0400

    added active and inactive style states for requestsbox component

--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -4,11 +4,11 @@ class RequestBox extends Component {
   render() {
     const { count, title } = this.props;
       return (
-        <div className='requests-box'>
+        <a onClick={() => console.log('Trying to run') } className='requests-box requests-box-inactive'>
           <div className='requests-box__count'>{count}</div>
           <div className='requests-box__title'>{title}</div>
           <div className='requests-box__point'></div>
-        </div>
+        </a>
       )
     }
   }
