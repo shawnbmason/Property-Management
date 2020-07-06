5b4321d8ff429144c770339e00e0d7b6405c3f4e
shawnbmason <s.mason1993@yahoo.com>
2020-07-06 18:56:22 -0400

    JSX for request item

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Button from '../button';
 import Icon from '../icon';
 
 class RequstsItem extends Component {
@@ -7,10 +8,21 @@ class RequstsItem extends Component {
     render() {
         return (
             <div className='requests-item'>
-                <div>
-                    <Icon icon='fas fa-exclamation-triangle'/>
+                <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
+                <div className='requests-item__title'>
+                    Yo my door fell down
                 </div>
-            </div>
+                <div className='requests-item__tenant-unit'>
+                    Max - Unit 115
+                </div>
+                <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+                <div className='requests-item__date'>
+                    09/12/93
+                </div>
+
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna move request item')}/>
+
+              </div>
         )
     }
 }
