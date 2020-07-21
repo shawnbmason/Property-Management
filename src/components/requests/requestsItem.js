17555b1e6c41f3d0b5d87c5b552fb81befb07bd3
shawnbmason <s.mason1993@yahoo.com>
2020-07-20 20:02:47 -0400

    put correct item count in boxes for requests feature

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -5,6 +5,8 @@ import Icon from '../icon';
 
 import AnimateHeight from 'react-animate-height';
 
+import { ROOT_URL } from '../../config';
+
 class RequstsItem extends Component {
 
   constructor() {
@@ -26,11 +28,12 @@ class RequstsItem extends Component {
     }
   }
     render() {
-        return (
+          const { _id, body, title, date, imageURL, status } = this.props;
+          const parsedDate = new Date(date);
             <div id='request-item' className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
                 <div className='requests-item__title'>
-                    <div className='requests-item__title__text'>Yo my door fell down</div>
+                    <div className='requests-item__title__text'>{title}</div>
                     <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                 </div>
                 <div className='requests-item__tenant-unit'>
@@ -38,7 +41,11 @@ class RequstsItem extends Component {
                 </div>
 
                 <div className='requests-item__date'>
-                    09/12/93
+                  { parsedDate.getMonth() + 1}
+                  /
+                  { parsedDate.getDate() }
+                  /
+                  { parsedDate.getFullYear() - 2000 }
                 </div>
                 <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna move request item')}/>
 
@@ -50,25 +57,16 @@ class RequstsItem extends Component {
                     <div className='item-description'>
                       <img
                         className='item-description__img'
-                        src='http://via.placeholder.com/160x94'
+                        src={`${ROOT_URL}/${imageUrl}`}
                       />
                       <p className='item-description__text'>
-                      An ab quid senserit, iis e fore nisi multos, id sunt fugiat a appellat sed se
-                      nulla exquisitaque. Sed aute dolore esse aliquip ne duis incurreret admodum se
-                      magna excepteur o despicationes ita iis est concursionibus. Hic cupidatat
-                      despicationes, commodo o aute cernantur ad sed offendit quo deserunt id nisi ea
-                      cernantur. Nostrud iis cillum te a elit o summis ne nam ubi minim laboris sed ea
-                      quae quae velit singulis.Qui quorum consequat. Est o ipsum duis nulla non
-                      arbitror tamen multos occaecat veniam si sed nisi deserunt, dolore et si cillum
-                      senserit. Pariatur ea legam ne ita ea enim officia si dolore e cupidatat, irure
-                      cupidatat quamquam.
+                        {body}
                       </p>
                     </div>
                   </AnimateHeight>
                 </div>
 
               </div>
-        )
     }
 }
 
